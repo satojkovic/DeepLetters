ecf56a07eb0e3f24e6b99422af56eed7b7a9709e
satojkovic <satojkovic@gmail.com>
2018-09-02 18:43:24 +0900

    Update layer definition

--- text_boxes.py
+++ text_boxes.py
@@ -4,7 +4,7 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
-INPUT_SIZE = 700
+INPUT_SIZE = 300
 INPUT_CH = 3
 
 
@@ -13,97 +13,133 @@ def forward(input_images):
     
     # VGG-16 (conv1_1 through conv4_3)
     with slim.arg_scope([slim.conv2d], kernel_size=(3, 3)):
-        # 700 x 700 x 3
+        # 300 x 300 x 3
         end_point = 'conv1_1'
         net = slim.conv2d(input_images, 64)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 700 x 700 x 64
+        # 300 x 300 x 64
         end_point = 'conv1_2'
         net = slim.conv2d(net, 64)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 700 x 700 x 64
+        # 300 x 300 x 64
         end_point = 'pool1'
         net = slim.max_pool2d(net, (2, 2), padding='SAME')
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 350 x 350 x 64
+        # 150 x 150 x 64
         end_point = 'conv2_1'
         net = slim.conv2d(net, 128)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 350 x 350 x 128
+        # 150 x 150 x 128
         end_point = 'conv2_2'
         net = slim.conv2d(net, 128)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 350 x 350 x 128
+        # 150 x 150 x 128
         end_point = 'pool2'
         net = slim.max_pool2d(net, (2, 2), padding='SAME')
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 175 x 175 x 128
+        # 75 x 75 x 128
         end_point = 'conv3_1'
         net = slim.conv2d(net, 256)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 175 x 175 x 256
+        # 75 x 75 x 256
         end_point = 'conv3_2'
         net = slim.conv2d(net, 256)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 175 x 175 x 256
+        # 75 x 75 x 256
         end_point = 'conv3_3'
         net = slim.conv2d(net, 256)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 175 x 175 x 256
+        # 75 x 75 x 256
         end_point = 'pool3'
         net = slim.max_pool2d(net, (2, 2), padding='SAME')
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 88 x 88 x 256
+        # 38 x 38 x 256
         end_point = 'conv4_1'
         net = slim.conv2d(net, 512)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 88 x 88 x 512
+        # 38 x 38 x 512
         end_point = 'conv4_2'
         net = slim.conv2d(net, 512)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 88 x 88 x 512
+        # 38 x 38 x 512
         end_point = 'conv4_3'
         net = slim.conv2d(net, 512)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 88 x 88 x 512
+        # 38 x 38 x 512
         end_point = 'pool4'
         net = slim.max_pool2d(net, (2, 2), padding='SAME')
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 44 x 44 x 512
+        # 19 x 19 x 512
         end_point = 'conv5_1'
         net = slim.conv2d(net, 512)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 44 x 44 x 512
+        # 19 x 19 x 512
         end_point = 'conv5_2'
         net = slim.conv2d(net, 512)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 44 x 44 x 512
+        # 19 x 19 x 512
         end_point = 'conv5_3'
         net = slim.conv2d(net, 512)
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 44 x 44 x 512
+        # 19 x 19 x 512
         end_point = 'pool5'
         net = slim.max_pool2d(net, (3, 3), stride=1, padding='SAME')
         end_points[end_point] = net
         print(end_point, ':', net.get_shape())
-        # 44 x 44 x 512
+        # 19 x 19 x 512
+        end_point = 'conv6'
+        net = slim.conv2d(net, 1024, rate=6)
+        end_points[end_point] = net
+        print(end_point, ':', net.get_shape())
+        # 19 x 19 x 1024
+        end_point = 'conv7'
+        net = slim.conv2d(net, 1024, kernel_size=[1, 1])
+        end_points[end_point] = net
+        print(end_point, ':', net.get_shape())
+        # 19 x 19 x 1024
+        end_point = 'conv8'
+        net = slim.conv2d(net, 256, kernel_size=[1, 1])
+        net = tf.pad(net, [[0, 0], [1,1], [1,1], [0, 0]])
+        net = slim.conv2d(net, 512, stride=2, padding='VALID')
+        end_points[end_point] = net
+        print(net.get_shape())
+        # 10 x 10 x 512
+        end_point = 'conv9'
+        net = slim.conv2d(net, 128, kernel_size=[1, 1])
+        net = tf.pad(net, [[0, 0], [1, 1], [1, 1], [0, 0]])
+        net = slim.conv2d(net, 256, stride=2, padding='VALID')
+        end_points[end_point] = net
+        print(net.get_shape())
+        # 5 x 5 x 256
+        end_point = 'conv10'
+        net = slim.conv2d(net, 128, kernel_size=[1, 1])
+        net = slim.conv2d(net, 256, padding='VALID')
+        end_points[end_point] = net
+        print(net.get_shape())
+        # 3 x 3 x 256
+        end_point = 'global'
+        net = slim.conv2d(net, 128, kernel_size=[1, 1])
+        net = slim.conv2d(net, 256, padding='VALID')
+        end_points[end_point] = net
+        print(net.get_shape())
+        # 1 x 1 x 256
 
     logits = net
     return logits, end_points
