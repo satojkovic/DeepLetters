28701faa01e7a2560cec1292b31c7b2ac852509b
satojkovic <satojkovic@gmail.com>
2019-10-12 17:14:04 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-import tensorflow as tf
-import deep_text
-
-
-def main():
-    with tf.Graph().as_default():
-        input_images = tf.placeholder(
-            tf.float32, 
-            shape=(None, deep_text.INPUT_SIZE, deep_text.INPUT_SIZE, deep_text.INPUT_CH),
-            name='input_images'
-        )
-
-        # DeepText model
-        print('** DeepText model **')
-        end_points = deep_text.forward(input_images)
-
+import cv2
 
 if __name__ == "__main__":
-    main()
+    # East model
+    print('** Load EAST model **')
+    net = cv2.dnn.readNet('frozen_east_text_detection.pb')
