73c66d7bc4b3710fda7621d744b9cdb454cf2ab3
satojkovic <satojkovic@gmail.com>
2019-10-15 19:56:20 +0900

    Text detection using OpenCV EAST model

--- deep_text.py
+++ deep_text.py
@@ -7,14 +7,33 @@ from model import CvEAST
 
 def parse_cmdline_flags():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_image', required=True, help='Path to input image')
+    parser.add_argument('--input', required=True, help='Path to input image or video file')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_cmdline_flags()
 
-    # Load the input image
-    img = cv2.imread(args.input_image)
-
-    # EAST model
+    # Load EAST model
     net = CvEAST('frozen_east_text_detection.pb')
+
+    # Open a video file or an image file
+    cap = cv2.VideoCapture(args.input if args.input else 0)
+
+    while cv2.waitKey(1) < 0:
+        has_frame, frame = cap.read()
+        if not has_frame:
+            cv2.waitKey(0)
+            break
+
+        ratio_w, ratio_h, indices, boxes = net.predict(frame)
+        for i in indices:
+            vertices = cv2.boxPoints(boxes[i[0]])
+            for j in range(4):
+                vertices[j][0] *= ratio_w
+                vertices[j][1] *= ratio_h
+            for j in range(4):
+                p1 = (vertices[j][0], vertices[j][1])
+                p2 = (vertices[(j + 1) % 4][0], vertices[(j + 1) % 4][1])
+                cv2.line(frame, p1, p2, (0, 255, 0), 1)
+
+        cv2.imshow('Text Detection', frame)
--- model.py
+++ model.py
@@ -1,11 +1,48 @@
 import cv2
+import math
 
 class CvEAST:
-    def __init__(self, pb_file, width=320, height=320):
+    def __init__(self, pb_file, width=320, height=320, conf_th=0.5, nms_th=0.4):
         self.width = width
         self.height = height
+        self.conf_th = conf_th
+        self.nms_th = nms_th
         self.layer_names = ["feature_fusion/Conv_7/Sigmoid", "feature_fusion/concat_3"]
         self.net = cv2.dnn.readNet(pb_file)
+        self.detections = []
+        self.confidences = []
+
+    def decode(self):
+        detections = []
+        confidences = []
+        height = self.scores.shape[2]
+        width = self.scores.shape[3]
+        for y in range(0, height):
+            scores_data = self.scores[0][0][y]
+            x0_data = self.geometry[0][0][y]
+            x1_data = self.geometry[0][1][y]
+            x2_data = self.geometry[0][2][y]
+            x3_data = self.geometry[0][3][y]
+            angles_data = self.geometry[0][4][y]
+            for x in range(0, width):
+                score = scores_data[x]
+                if score < self.conf_th:
+                    continue
+                offset_x = x * 4.0
+                offset_y = y * 4.0
+                angle = angles_data[x]
+                cos_a = math.cos(angle)
+                sin_a = math.sin(angle)
+                h = x0_data[x] + x2_data[x]
+                w = x1_data[x] + x3_data[x]
+                offset = ([offset_x + cos_a * x1_data[x] + sin_a * x2_data[x], offset_y - sin_a * x1_data[x] + cos_a * x2_data[x]])
+
+                p1 = (-sin_a * h + offset[0], -cos_a * h + offset[1])
+                p3 = (-cos_a * w + offset[0], sin_a * w + offset[1])
+                center = (0.5 * (p1[0] + p3[0]), 0.5 * (p1[1] + p3[1]))
+                detections.append((center, (w, h), -1 * angle * 180.0 / math.pi))
+                confidences.append(float(score))
+        return [detections, confidences]
 
     def predict(self, image):
         org_image = image.copy()
@@ -19,4 +56,12 @@ class CvEAST:
             image, 1.0, (self.width, self.height),
             (123.68, 116.78, 103.94), swapRB=True, crop=False)
         self.net.setInput(blob)
-        (self.scores, self.geometry) = self.net.forward(self.layer_names)
+        outs = self.net.forward(self.layer_names)
+        self.scores = outs[0]
+        self.geometry = outs[1]
+
+        [self.boxes, self.confidences] = self.decode()
+
+        indices = cv2.dnn.NMSBoxesRotated(self.boxes, self.confidences, self.conf_th, self.nms_th)
+
+        return self.ratio_w, self.ratio_h, indices, self.boxes
