a801998ee82b12dfeeb1eb376012dbdcf3b8bbbb
satojkovic <satojkovic@gmail.com>
2020-01-27 21:48:31 +0900

    Add CRNN model

--- model.py
+++ model.py
@@ -3,6 +3,36 @@ import math
 import pytesseract
 from imutils.object_detection import non_max_suppression
 import numpy as np
+import string
+
+from keras.layers import Conv2D, Input, MaxPool2D, BatchNormalization, LSTM, Lambda, Bidirectional, Dense
+from keras.models import Model
+import keras.backend as K
+
+class CRNN:
+    def __init__(self, width=128, height=32):
+        self.char_list = string.ascii_letters + string.digits
+        self.width = width
+        self.height = height
+        self.inputs = Input(shape=(self.height, self.width, 1))
+        self.conv_1 = Conv2D(64, (3, 3), activation='relu', padding='same')(self.inputs)
+        self.pool_1 = MaxPool2D(pool_size=(2, 2), strides=2)(self.conv_1)
+        self.conv_2 = Conv2D(128, (3, 3), activation='relu', padding='same')(self.pool_1)
+        self.pool_2 = MaxPool2D(pool_size=(2, 2), strides=2)(self.conv_2)
+        self.conv_3 = Conv2D(256, (3, 3), activation='relu', padding='same')(self.pool_2)
+        self.conv_4 = Conv2D(256, (3, 3), activation='relu', padding='same')(self.conv_3)
+        self.pool_4 = MaxPool2D(pool_size=(2, 1))(self.conv_4)
+        self.conv_5 = Conv2D(512, (3,3), activation='relu', padding='same')(self.pool_4)
+        self.batch_norm_5 = BatchNormalization()(self.conv_5)
+        self.conv_6 = Conv2D(512, (3,3), activation='relu', padding='same')(self.batch_norm_5)
+        self.batch_norm_6 = BatchNormalization()(self.conv_6)
+        self.pool_6 = MaxPool2D(pool_size=(2, 1))(self.batch_norm_6)
+        self.conv_7 = Conv2D(512, (2,2), activation='relu')(self.pool_6)
+        self.squeezed = Lambda(lambda x: K.squeeze(x, 1))(self.conv_7)
+        self.blstm_1 = Bidirectional(LSTM(128, return_sequences=True, dropout=0.2))(self.squeezed)
+        self.blstm_2 = Bidirectional(LSTM(128, return_sequences=True, dropout=0.2))(self.blstm_1)
+        self.outputs = Dense(len(self.char_list)+1, activation='softmax')(self.blstm_2)
+        self.model = Model(self.inputs, self.outputs)
 
 class CvEAST:
     def __init__(self, pb_file, width=320, height=320, conf_th=0.5, nms_th=0.4, roi_pad=0.0):
