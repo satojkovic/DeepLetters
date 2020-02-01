458f1a5fa847ce145d43c6d038b62c78c75ac6b3
satojkovic <satojkovic@gmail.com>
2020-02-01 22:44:24 +0900

    Use CRNN model

--- deep_letters.py
+++ deep_letters.py
@@ -3,18 +3,30 @@
 
 import cv2
 import argparse
-from model import CvEAST
-import tensorflow as tf
 import os
-from object_detection.utils import ops as utils_ops
 import numpy as np
-import pytesseract
+import sys
+from pathlib import Path
+from PIL import Image
+import string
+
+import tensorflow as tf
+from object_detection.utils import ops as utils_ops
+
+sys.path.append(os.path.join(Path().resolve(), '..', 'crnn.pytorch'))
+from models.crnn import CRNN
+import utils
+import dataset
+from torchvision.transforms import transforms
+from torch.autograd import Variable
+import torch
 
 def parse_cmdline_flags():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', required=True, help='Path to input image or video file')
-    parser.add_argument('--inference_graph_dir_path', required=True, help='Path to inference graph directory')
+    parser.add_argument('--detection_model_path', required=True, help='Path to text detection model(.pb)')
     parser.add_argument('--detection_th', type=float, required=True, help='Threshold value for detection boxes')
+    parser.add_argument('--recognition_model_path', required=True, help='Path to text recognition model(.pth)')
     return parser.parse_args()
 
 def run_inference_for_single_image(image, graph):
@@ -67,13 +79,23 @@ if __name__ == "__main__":
     args = parse_cmdline_flags()
 
     # Load SSD model
-    PATH_TO_FROZEN_GRAPH = os.path.join(args.inference_graph_dir_path, 'frozen_inference_graph.pb')
+    PATH_TO_FROZEN_GRAPH = args.detection_model_path
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
         with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as f:
             od_graph_def.ParseFromString(f.read())
             tf.import_graph_def(od_graph_def, name='')
+
+    # Load CRNN model
+    alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+    crnn = CRNN(32, 1, 37, 256)
+    if torch.cuda.is_available():
+        crnn = crnn.cuda()
+    crnn.load_state_dict(torch.load(args.recognition_model_path))
+    converter = utils.strLabelConverter(alphabet)
+    transformer = dataset.resizeNormalize((100, 32))
+    crnn.eval()
 
     # Open a video file or an image file
     cap = cv2.VideoCapture(args.input if args.input else 0)
@@ -85,19 +107,30 @@ if __name__ == "__main__":
             break
 
         im_height, im_width, _ = frame.shape
-        frame = np.expand_dims(frame, axis=0)
-        output_dict = run_inference_for_single_image(frame, detection_graph)
+        tf_frame = np.expand_dims(frame, axis=0)
+        output_dict = run_inference_for_single_image(tf_frame, detection_graph)
 
         for i in range(output_dict['num_detections']):
             if output_dict['detection_scores'][i] < args.detection_th:
                 continue
             ymin, xmin, ymax, xmax = output_dict['detection_boxes'][i]
             left, right, top, bottom = int(xmin * im_width), int(xmax * im_width), int(ymin * im_height), int(ymax * im_height)
-            cv2.rectangle(frame[0], (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # convert to pil
+            box_frame = cv2.cvtColor(frame[top:bottom, left:right], cv2.COLOR_BGR2GRAY)
+            pil_box_frame = Image.fromarray(box_frame)
 
             # Text recognition
-            text = pytesseract.image_to_string(frame[0][top:bottom, left:right], config=r'--psm 7')
-            text = ''.join([c if ord(c) < 128 else '' for c in text]).strip()
-            cv2.putText(frame[0], text, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
+            pil_box_frame = transformer(pil_box_frame)
+            pil_box_frame = pil_box_frame.view(1, *pil_box_frame.size())
+            pil_box_frame = Variable(pil_box_frame)
+            preds = crnn(pil_box_frame)
+            _, preds = preds.max(2)
+            preds = preds.transpose(1, 0).contiguous().view(-1)
+            preds_size = preds_size = Variable(torch.IntTensor([preds.size(0)]))
+            raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
+            sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+            cv2.putText(frame, sim_pred, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 2)
 
-        cv2.imshow('Text Detection', frame[0])
+        cv2.imshow('Text Detection', frame)
